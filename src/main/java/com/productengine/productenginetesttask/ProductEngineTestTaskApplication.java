0e61c7b101a09c609948363371b1bdb5b0077498
viktoriiakotovets <v.kotovets@do-it.co>
2022-08-11 16:39:40 +0300

    initial commit

---
+++ src/main/java/com/productengine/productenginetesttask/ProductEngineTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.productengine.productenginetesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductEngineTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductEngineTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/productengine/productenginetesttask/ProductEngineTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productengine.productenginetesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductEngineTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
